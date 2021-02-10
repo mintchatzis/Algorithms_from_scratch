0e1d86c32b5fa02446f125c25c55b6a5eccad632
Michail Chatzis <michatzis@gmail.com>
2021-02-10 16:45:24 +0000

    df_search working

--- CS core algorithms/Graph_Algos/df_search.py
+++ CS core algorithms/Graph_Algos/df_search.py
@@ -6,22 +6,21 @@ def df_search(graph,item, root = None):
     graph.init_visited()
     
     def inner_rec(vertex):
-        if graph.data[vertex] == item:
+        if graph.get_data(vertex) == item:
             return vertex
+        graph.set_visited(vertex,True)
         
-        neighbors = graph[vertex]
-        for neighbor in neighbors:
-            if not graph.visited[vertex]:
-                inner_rec(neighbor)
+        neighbors = graph.get_neighbors(vertex)
+        for neighbor_ver in neighbors:
+            if not graph.get_visited(neighbor_ver):
+                inner_rec(neighbor_ver)
+        return None     #no more neighbors to explore
     
-    for node in graph.graph.keys():
-        result = inner_rec(node)
-
+    for vertex in graph.get_vertices():
+        result = inner_rec(vertex)
         if result is not None:
             return result
     return None
-    
-    
 
 
 if __name__ == "__main__":
@@ -36,8 +35,21 @@ if __name__ == "__main__":
     extra_connections = [('g','h'),
                          ('h','f')]
     
+    
+    
     g_undir = Graph(init_connections)
     g_dir = Graph(init_connections,directed=True)
+    
+    i = 0
+    for vertex in g_undir.get_vertices():
+        g_undir.set_data(vertex,i)
+        i += 1
+    print(g_undir.get_all_data())
+        
+    
+    #print(g_undir.graph)
+    print(df_search(g_undir,5))
+    
     
     
 
@@ -46,3 +58,4 @@ if __name__ == "__main__":
                 
             
     
+ 
